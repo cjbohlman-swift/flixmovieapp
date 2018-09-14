eddbe6f18775726e9e2bd61a8af0440efdf3286b
Mely Bohlman <melykiwi@Melys-MacBook-Air.local>
2018-09-13 21:35:51 -0700

    Added images

---
+++ flixster/MovieCell.swift
@@ -0,0 +1,30 @@
+//
+//  MovieCell.swift
+//  flixster
+//
+//  Created by Mely Bohlman on 9/12/18.
+//  Copyright © 2018 Chris Bohlman. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var overviewLabel: UILabel!
+    
+    @IBOutlet weak var movieImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
